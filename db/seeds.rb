4b872e24705e2304900a87537e1e784c2a0e1866
Dylon M <dylonmura@gmail.com>
2023-05-25 16:59:18 +0100

    movie 3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+require "json"
+
+Movie.destroy_all
+
+# 50.times do
+#   Movie.create(
+#     title: Faker::Movie.title,
+#     overview: Faker::Quote.matz,
+#     rating: rand(0.0..10.0).round(1)
+#   )
+# end
+
+url = "https://tmdb.lewagon.com/movie/top_rated"
+img_url = "https://image.tmdb.org/t/p/w500"
+
+json = JSON.parse(URI.open(url).read)
+
+# puts json["results"][1]
+
+json["results"].each do |el|
+  Movie.create(
+    title: el["original_title"],
+    overview: el["overview"],
+    rating: el["vote_average"],
+    poster_url: img_url + el["poster_path"]
+  )
+end
